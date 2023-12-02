2044901f6c8214433e05f3feeaf3aa664ab9c972
rekla abderrahim <reklaabderrahim@hotmail.com>
2023-12-02 09:13:49 +0100

    passer sur kafka pour les mails

---
+++ src/main/java/fr/mr_market/mr_notification/kafka/MailMessageConsumer.java
@@ -0,0 +1,26 @@
+package fr.mr_market.mr_notification.kafka;
+
+import fr.mr_market.mr_notification.model.Mail;
+import fr.mr_market.mr_notification.model.MailRequest;
+import fr.mr_market.mr_notification.service.MailService;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class MailMessageConsumer {
+
+    private final MailService mailService;
+
+    @KafkaListener(topics = "mail-register", groupId = "register_consumer_group")
+    public void listen(MailRequest request) throws MessagingException {
+        log.debug("Mail message received: {}", request);
+        final Mail mail = mailService.create(request.recipients(), request.mailType(), request.callbackUrl());
+        mailService.sendHTMLEmail(mail);
+    }
+
+}
